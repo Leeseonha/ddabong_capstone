521df5c27e025a4ccd40872b63461d16b59797cc
JeongEun <lisasje25@gmail.com>
2020-09-22 23:30:58 +0900

    post 디비 구성 완료
    카카오에서 user 끌어오기 가능
    아마,,?

--- ddabong/contact/models.py
+++ ddabong/contact/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+# from myvenv.socialaccount.models import SocialAccount
+# from accounts.models import SocialAccount
 
+
+
+
+from django.contrib.auth import authenticate
+from django.contrib.sites.models import Site
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import PermissionDenied
+from django.db import models
+from django.utils.crypto import get_random_string
+from django.utils.encoding import force_str
+from django.utils.translation import gettext_lazy as _
+import allauth.app_settings
+from allauth.account.models import EmailAddress
+from allauth.account.utils import get_next_redirect_url, setup_user_email
+from allauth.utils import get_user_model
 # Create your models here.
 class Post(models.Model):
     B_TYPES = (
@@ -36,7 +53,7 @@ class Post(models.Model):
         ('Donor','공여자'),
         ('recipient','수혜자'),
     )
-
+    user = models.ForeignKey(allauth.app_settings.USER_MODEL,on_delete=models.CASCADE)
     post_type = models.CharField(max_length= 50, choices= Post_type)
     title = models.CharField(max_length= 200)
     pub_date = models.DateTimeField('date published')
--- ddabong/ddabong/settings.py
+++ ddabong/ddabong/settings.py
@@ -102,7 +102,7 @@ AUTHENTICATION_BACKENDS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
