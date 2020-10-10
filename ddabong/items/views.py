7217d682f8f0b2568afce00561b884819266f2a1
Leeseonha <jmtkdsh@gmail.com>
2020-10-10 18:53:03 +0900

    다시 수정

--- ddabong/items/views.py
+++ ddabong/items/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def items(request):
-    return render(request, 'items.html')zz
+    return render(request, 'items.html')
